1d54d115a170bf1785a8e5fd9aa5f2aa569ee703
gi0SAN <sanagustingio@gmail.com>
2024-10-10 15:58:25 -0400

    Update recursionFactorial.java

--- recursionFactorial.java
+++ recursionFactorial.java
@@ -7,6 +7,7 @@
  */
 import java.util.*;
 public class recursionFactorial {
+	//Method to find a a factorial
 	static int factorial (int n)
 	{
 		if (n == 0) {
@@ -18,12 +19,15 @@ public class recursionFactorial {
 		}
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Scanner is called
 		Scanner reader = new Scanner (System.in);
+		//Ask the user to input a random integer
 		System.out.print("Enter a non-negative integer: " );
+		// User can input a non negative integer
 		int n = reader.nextInt();
 		//Display factorial
 		System.out.println("Factorial of " + n + " is " + factorial(n));
+		//Facotrial is returned 
 
 	}
 
